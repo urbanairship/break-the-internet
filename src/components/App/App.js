efc6dbc2ca14022671253e7a6a0fc4150809350e
Sofiane Abdelfettah <sofianealternance@gmail.com>
2019-09-24 17:56:40 +0200

    layout added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,97 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Row, Card } from "antd";
 import "./App.scss";
 
 function App() {
-  const { Header, Footer, Sider, Content } = Layout;
+  const { Sider } = Layout;
 
   return (
-    <div className="App">
-      <Layout>
-        <Header>Header</Header>
-        <Layout>
-          <Content>Content</Content>
-          <Sider>Sider</Sider>
-        </Layout>
-        <Footer>Footer</Footer>
-      </Layout>
-    </div>
+    <Layout className="app-container1">
+      <Row type="flex" style={{margin: '2%'}}>
+        <Row type="flex" justify="space-between" style={{marginBottom: '1%', width: "94%"}}>
+          <Card style={{ width: 300 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+          <Card style={{ width: 300 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+          <Card style={{ width: 300 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+          <Card style={{ width: 300 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </Row>
+        <Row type="flex" className="carrousel-container">
+          <Row type="flex" className="carrousel">
+            <Card style={{ height: 250, width: "100%" }}>
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+            <Row type="flex" justify="space-between" className="card-events">
+              <Card style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+              <Card style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+              <Card style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+              <Card style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </Row>
+            <Row type="flex" justify="space-between" className="card-events">
+              <Card style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+              <Card style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+              <Card style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+              <Card style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </Row>
+          </Row>
+          <Row type="flex" className="sider-container" style={{marginLeft: '1%'}}>
+            <Sider width="400" className="sider">
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Sider>
+          </Row>
+        </Row>
+      </Row>
+    </Layout>
   );
 }
 
