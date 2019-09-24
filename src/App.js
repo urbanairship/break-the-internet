c7a2ca4e26178c10aa770dd0edf3d780dbf97928
Thang Huynh <thang.edreams@gmail.com>
2019-09-24 14:12:51 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout } from "antd";
+
+import "./App.css";
 
 function App() {
+  const { Header, Footer, Sider, Content } = Layout;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header>Header</Header>
+        <Layout>
+          <Content>Content</Content>
+          <Sider>Sider</Sider>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
     </div>
   );
 }
