f6dbbf89ee6cf00a88835d495652a2b65d80031f
sofiane <sofianealternance@gmail.com>
2019-09-25 00:41:22 +0100

    fix carousel width issue

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Layout, Row, Card } from "antd";
+import NewsCard from "../NewsCard/NewsCard";
+import { Layout, Row, Carousel } from "antd";
 import "./App.scss";
 
 function App() {
@@ -7,82 +8,72 @@ function App() {
 
   return (
     <Layout className="app-container1">
-      <Row type="flex" style={{margin: '2%'}}>
-        <Row type="flex" justify="space-between" style={{marginBottom: '1%', width: "94%"}}>
-          <Card style={{ width: 300 }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
-          </Card>
-          <Card style={{ width: 300 }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
-          </Card>
-          <Card style={{ width: 300 }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
-          </Card>
-          <Card style={{ width: 300 }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
-          </Card>
+      <Row type="flex" style={{ margin: "20px" }}>
+        <Row
+          type="flex"
+          justify="space-between"
+          style={{ marginBottom: "20px", width: "100%", flexWrap: "nowrap" }}
+        >
+          <NewsCard />
+          <NewsCard />
+          <NewsCard />
+          <NewsCard />
         </Row>
-        <Row type="flex" className="carrousel-container">
-          <Row type="flex" className="carrousel">
-            <Card style={{ height: 250, width: "100%" }}>
-              <p>Card content</p>
-              <p>Card content</p>
-              <p>Card content</p>
-            </Card>
-            <Row type="flex" justify="space-between" className="card-events">
-              <Card style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-              </Card>
-              <Card style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-              </Card>
-              <Card style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-              </Card>
-              <Card style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-              </Card>
+        <Row
+          type="flex"
+          className="carrousel-container"
+          style={{ flexWrap: "nowrap" }}
+        >
+          <Row className="carrousel" type="flex">
+            <Carousel
+              style={{
+                width: "1120px",
+                background: "black",
+                height: "160px",
+                display: "block"
+              }}
+            >
+              <div>
+                <h3>1</h3>
+              </div>
+              <div>
+                <h3>2</h3>
+              </div>
+              <div>
+                <h3>3</h3>
+              </div>
+              <div>
+                <h3>4</h3>
+              </div>
+            </Carousel>
+            <Row
+              type="flex"
+              justify="space-between"
+              className="card-events"
+              style={{ flexWrap: "nowrap" }}
+            >
+              <NewsCard />
+              <NewsCard />
+              <NewsCard />
+              <NewsCard />
             </Row>
-            <Row type="flex" justify="space-between" className="card-events">
-              <Card style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-              </Card>
-              <Card style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-              </Card>
-              <Card style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-              </Card>
-              <Card style={{ width: 300 }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
-              </Card>
+            <Row
+              type="flex"
+              justify="space-between"
+              className="card-events"
+              style={{ flexWrap: "nowrap" }}
+            >
+              <NewsCard />
+              <NewsCard />
+              <NewsCard />
+              <NewsCard />
             </Row>
           </Row>
-          <Row type="flex" className="sider-container" style={{marginLeft: '1%'}}>
+          <Row
+            type="flex"
+            className="sider-container"
+            style={{ marginLeft: "1%", flexWrap: "nowrap" }}
+          >
             <Sider width="400" className="sider">
               <p>Card content</p>
               <p>Card content</p>
---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "antd";
+
+const NewsCard = () => (
+  <Card style={{ width: "250px" }}>
+    <p>Card content</p>
+    <p>Card content</p>
+    <p>Card content</p>
+  </Card>
+);
+
+export default NewsCard;
