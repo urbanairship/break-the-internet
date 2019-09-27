526dea8c290ac881a260c92931774204af28b830
Sofiane Abdelfettah <sofianealternance@gmail.com>
2019-09-27 15:48:15 +0200

    add sidemenu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import NewsCard from "../NewsCard/NewsCard";
 import { CarouselNews } from "../Carousel";
 import { ClockNews } from "../ClockNews";
 import { SiderContent } from "../Sider";
-import { Layout, Row, Col } from "antd";
+import { Layout, Row, Col, Icon } from "antd";
 import logo from "../../logo.png";
 import "./App.scss";
 
 function App() {
   const { Footer } = Layout;
+  const [getSider, setSider] = useState(false);
+  const isSiderOpen = getSider ? { width: "250px" } : { width: "0px" };
   return (
     <div>
+      <div className="side-menu">
+        <span
+          style={{ fontSize: "30px", cursor: "pointer" }}
+          onClick={() => setSider(true)}
+        >
+          <Icon type="menu" />
+        </span>
+        <div id="mySidenav" className="sidenav" style={isSiderOpen}>
+        <Icon type="close"  className="closebtn" onClick={() => setSider(false)} />
+          <a href="#">Articles</a>
+          <a href="#">Events</a>
+          <a href="#">Favorites</a>
+          <a href="#">Editor</a>
+          <a href="#">Settings</a>
+        </div>
+      </div>
       <Row>
         <Row type="flex" justify="center">
           <img src={logo} alt="logo" />
@@ -39,7 +57,7 @@ function App() {
               <Col span={4} style={{ margin: 20 }}>
                 <ClockNews
                   cardTitle="San Francisco"
-                  timezone="America/New_York"
+                  timezone="America/Los_Angeles"
                 />
               </Col>
             </Row>
